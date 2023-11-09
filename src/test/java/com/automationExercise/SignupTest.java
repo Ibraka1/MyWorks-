0ce50ff64d54f293ffd460424db9d67902bbb320
Ibraka1 <ibraka48@gmail.com>
2023-11-09 23:14:11 +0300

    Test Start

--- src/test/java/com/automationExercise/SignupTest.java
+++ src/test/java/com/automationExercise/SignupTest.java
@@ -1,6 +1,6 @@
 package com.automationExercise;
 
-import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 
 
@@ -8,10 +8,27 @@ public class SignupTest {
 
     WebDriver driver;
 
-
-
-    @Before
-    public void setUp() throws Exception {
+    @Test
+    public void test01() throws Exception {
+        //Go to given URL https://www.automationexercise.com/
+        driver.get("https://www.automationexercise.com");
+        //Click on the Signup/Login button
+        //Confirm that Login page has opened
+        //Write your Name in section New User Signup! -- Ibrahim
+        //Write yor Email Address in section New User Signup! --ibraka48@gmail.com
+        //Click on Signup button
+        //Click on Mr. for Title
+        //Write value to Password section -- Selenium1234
+        //Write First name to box in ADDRESS INFORMATION section -- Ibrahim
+        //"Write Last name to box in ADDRESS INFORMATION section --  Akar
+        //"Write Address to box in ADDRESS INFORMATION section -- Dulkadiroglu
+        //Select Country from dropdwon menü --Canada
+        //Write State to box in ADDRESS INFORMATION section --Kahramanmaras
+        //Write City to box in ADDRESS INFORMATION section -- Kahramanmaras
+        //Write Zipcode to box in ADDRESS INFORMATION section -  46080
+        //Write Mobile Number to box in ADDRESS INFORMATION section -- 5451234578
+        //Click on Create Account button
+        //Confirm  created account
 
     }
 }
--- src/test/java/com/utulities/TestBase.java
+++ src/test/java/com/utulities/TestBase.java
@@ -1,4 +1,34 @@
 package com.utulities;
 
-public class TestBase {
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public abstract class TestBase {
+    protected WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+    public void waitForSecond(int saniye) throws InterruptedException {
+        Thread.sleep(saniye*1000);
+
+    }
+
+
 }
