5b7e766f6613e53ba1851e18d1fa3e6e2dbcf39c
Ibraka1 <ibraka48@gmail.com>
2023-11-09 21:53:16 +0300

    TestBase Created

--- src/test/java/com/automationExercise/SignupTest.java
+++ src/test/java/com/automationExercise/SignupTest.java
@@ -1,4 +1,17 @@
 package com.automationExercise;
 
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+
+
 public class SignupTest {
+
+    WebDriver driver;
+
+
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
 }
---
+++ src/test/java/com/utulities/TestBase.java
@@ -0,0 +1,4 @@
+package com.utulities;
+
+public class TestBase {
+}
